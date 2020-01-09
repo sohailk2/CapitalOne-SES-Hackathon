d3d6a2f6a5509a06f2c586d0872b02028905d8f0
kevin-chen <kc3585@nyu.edu>
2020-01-09 14:52:25 -0500

    setup features

--- cashback/project/views.py
+++ cashback/project/views.py
@@ -2,11 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import firebase
 
-firebase = firebase.FirebaseApplication('https://your_storage.firebaseio.com', None)
+# firebase = firebase.FirebaseApplication('https://your_storage.firebaseio.com', None)
 
 def home(request):
     return render(request, 'project/home.html')
 
 def firebase_test():
-    data.get("restart", "name")
-    data.put('restart', 'triggeredPressed', False)
+    pass
+    # data.get("restart", "name")
+    # data.put('restart', 'triggeredPressed', False)
