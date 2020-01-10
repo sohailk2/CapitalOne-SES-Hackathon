8e7763cc3de982b512121b7f45cf2ed5386c356b
Sohail Khan <sohailk2@illinois.edu>
2020-01-10 09:14:29 -0500

    added icons

--- cashback/cashback/settings.py
+++ cashback/cashback/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,4 +125,4 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
