d22fd5b14665f473832d43b108a44615e8a07be4
Sohail Khan <sohailk2@illinois.edu>
2020-01-10 05:59:18 -0500

    url update

--- cashback/project/urls.py
+++ cashback/project/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.login),
+    path('login', views.login),
     path('', views.home),
+    path('/', views.home),
     path('coins', views.getCoins),
     path('mapData/<lat>/<long>/<query>', views.getMapData),
     path('getCards/<latitude>/<longitude>/<query>', views.getCardsForTransaction),
