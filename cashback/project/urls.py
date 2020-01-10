07bdf8dacbf5e4f62eff50664cdbf624d3e7139b
Sohail Khan <sohailk2@illinois.edu>
2020-01-10 05:51:07 -0500

    requirements.txt

--- cashback/project/urls.py
+++ cashback/project/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login),
-    path('home', views.home),
+    # path('', views.login),
+    path('', views.home),
     path('coins', views.getCoins),
     path('mapData/<lat>/<long>/<query>', views.getMapData),
     path('getCards/<latitude>/<longitude>/<query>', views.getCardsForTransaction),
